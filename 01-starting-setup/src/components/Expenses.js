f00eaf541a17fd138a13fbc480946b48dfad7699
tintoll <belselios@gmail.com>
2022-10-21 08:53:01 +0900

    Card 컴포넌트 생성해서 CSS 중복 제거

--- 01-starting-setup/src/components/Expenses.js
+++ 01-starting-setup/src/components/Expenses.js
@@ -1,18 +1,20 @@
-import './Expenses.css'
-import ExpenseItem from './ExpeseItem';
+import Card from "./Card";
+import "./Expenses.css";
+import ExpenseItem from "./ExpeseItem";
 
 function Expenses(props) {
-    return (
-        <div className="expenses">
-            {props.items.map(expense =>
-                <ExpenseItem
-                    key={expense.id}
-                    title={expense.title}
-                    amout={expense.amount}
-                    date={expense.date}
-                />)}
-        </div>
-    )
+  return (
+    <Card className="expenses">
+      {props.items.map((expense) => (
+        <ExpenseItem
+          key={expense.id}
+          title={expense.title}
+          amout={expense.amount}
+          date={expense.date}
+        />
+      ))}
+    </Card>
+  );
 }
 
 export default Expenses;
--- 01-starting-setup/src/components/ExpeseItem.js
+++ 01-starting-setup/src/components/ExpeseItem.js
@@ -1,16 +1,16 @@
 import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-
   return (
-    <div className="expense-item">
+    <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h1>{props.title}</h1>
         <div className="expense-item__price">${props.amout}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
