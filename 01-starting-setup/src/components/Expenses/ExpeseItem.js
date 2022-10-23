2a5820f794d24daa9d54f8d57218a6f306766551
tintoll <belselios@gmail.com>
2022-10-24 07:00:59 +0900

    이벤트 설정

--- 01-starting-setup/src/components/Expenses/ExpeseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpeseItem.js
@@ -3,6 +3,11 @@ import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+
+  const clickHandler = () => {
+    console.log('Clicked!!!')
+  }
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
@@ -10,6 +15,7 @@ const ExpenseItem = (props) => {
         <h1>{props.title}</h1>
         <div className="expense-item__price">${props.amout}</div>
       </div>
+      <button onClick={clickHandler}>Click</button>
     </Card>
   );
 }
