1b221d9ca1d347db08c9ab6a2a58d308f7b654e0
tintoll <belselios@gmail.com>
2022-10-24 08:51:15 +0900

    state 사용법

--- 01-starting-setup/src/components/Expenses/ExpeseItem.js
+++ 01-starting-setup/src/components/Expenses/ExpeseItem.js
@@ -1,23 +1,26 @@
+import React, { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
 
   const clickHandler = () => {
-    console.log('Clicked!!!')
-  }
+    setTitle("Updated!");
+    console.log(title); // 변경된 타이틀로 보이지 않는다.
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h1>{props.title}</h1>
+        <h1>{title}</h1>
         <div className="expense-item__price">${props.amout}</div>
       </div>
       <button onClick={clickHandler}>Click</button>
     </Card>
   );
-}
+};
 
 export default ExpenseItem;
