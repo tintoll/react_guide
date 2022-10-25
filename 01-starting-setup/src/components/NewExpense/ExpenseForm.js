f4db0f61bcb36753e98afa5655bc6f03873b87a1
tintoll <belselios@gmail.com>
2022-10-26 08:38:36 +0900

    state - 양방향 바인딩 처리

--- 01-starting-setup/src/components/NewExpense/ExpenseForm.js
+++ 01-starting-setup/src/components/NewExpense/ExpenseForm.js
@@ -1,60 +1,105 @@
-import React, { useState } from 'react';
-import './ExpenseForm.css'
+import React, { useState } from "react";
+import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-    // const [enteredTitle, setEnteredTitle] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [enteredDate, setEnteredDate] = useState('');
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
 
-    const [userInput, setUserInput] = useState({
-        enteredTitle: '',
-        enteredAmount: '',
-        enteredDate: ''
-    })
+  //   const [userInput, setUserInput] = useState({
+  //     enteredTitle: "",
+  //     enteredAmount: "",
+  //     enteredDate: "",
+  //   });
 
-    const titleChangeHandler = (event) => {
-        // setEnteredTitle(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredTitle: event.target.value
-        })
-    }
-    const amoutChangeHandler = (event) => {
-        // setEnteredAmount(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredAmount: event.target.value
-        })
-    }
-    const dateChangeHandler = (event) => {
-        // setEnteredDate(event.target.value);
-        setUserInput({
-            ...userInput,
-            enteredDate: event.target.value
-        })
-    }
+  const titleChangeHandler = (event) => {
+    setEnteredTitle(event.target.value);
 
-    return (
-        <form>
-            <div className="new-expense__controls">
-                <div className='new-expense__control'>
-                    <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
-                </div>
-                <div className='new-expense__control'>
-                    <label>Amount</label>
-                    <input type="number" min="0.1" step="0.1" onChange={amoutChangeHandler} />
-                </div>
-                <div className='new-expense__control'>
-                    <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
-                </div>
-            </div>
-            <div className='new-expense__actions'>
-                <button type='submit'>Add Expense</button>
-            </div>
-        </form>
-    )
-}
+    // userInput의 스냅샷을 가져와야되는데 가장 최신것을 가져오지 못하는 경우도 있다.
+    // 그래서 아래의 함수형태를 사용해야한다.
+    // setUserInput({
+    //   ...userInput,
+    //   enteredTitle: event.target.value,
+    // });
+    // setUserInput((prevState) => {
+    //   return {
+    //     ...prevState,
+    //     enteredTitle: event.target.value,
+    //   };
+    // });
+  };
+  const amoutChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    //     setUserInput({
+    //       ...userInput,
+    //       enteredAmount: event.target.value,
+    //     });
+  };
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   enteredDate: event.target.value,
+    // });
+  };
+
+  const submitHandler = (event) => {
+    // submit의 기본 행동을 막아준다.
+    // 서버를 호출하면서 화면이 갱신되는 기본 행동
+    event.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+
+    console.log(expenseData);
+
+    // 각 input의 값을 초기화할때 value에 state값을 양방향 바인딩 시키고
+    // state 값을 초기화 해준다
+    setEnteredTitle("");
+    setEnteredAmount("");
+    setEnteredDate("");
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <div className="new-expense__controls">
+        <div className="new-expense__control">
+          <label>Title</label>
+          <input
+            type="text"
+            value={enteredTitle}
+            onChange={titleChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input
+            type="number"
+            min="0.1"
+            step="0.1"
+            value={enteredAmount}
+            onChange={amoutChangeHandler}
+          />
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-expense__actions">
+        <button type="submit">Add Expense</button>
+      </div>
+    </form>
+  );
+};
 
 export default ExpenseForm;
