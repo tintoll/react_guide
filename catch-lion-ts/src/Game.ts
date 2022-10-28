2969e3d9b9cfd6724b2140ac900295a5ab61fc58
tintoll <belselios@gmail.com>
2022-10-28 19:43:32 +0900

    타입스크립트 - 보드판에 플레이어 및 피스 적용

--- catch-lion-ts/src/Game.ts
+++ catch-lion-ts/src/Game.ts
@@ -1,7 +1,16 @@
-import { Board, DeadZone } from './Board';
+import { Board, Cell, DeadZone } from './Board';
+import { Player, PlayerType } from './Player';
 
 export class Game {
-  readonly board = new Board();
+  private selectedCell: Cell;
+  private turn = 0;
+  private currentPlayer: Player;
+  private gameInfoEl = document.querySelector('.alert');
+  private state: 'STARTED' | 'END';
+
+  readonly upperPlayer = new Player(PlayerType.UPPER);
+  readonly lowerPlayer = new Player(PlayerType.LOWER);
+  readonly board = new Board(this.upperPlayer, this.lowerPlayer);
   readonly upperDeadZone = new DeadZone('UPPER');
   readonly lowerDeadZone = new DeadZone('LOWER');
 
@@ -9,5 +18,30 @@ export class Game {
     const boardContainer = document.querySelector('.board-container');
     boardContainer?.firstChild?.remove();
     boardContainer?.appendChild(this.board._el);
+
+    this.currentPlayer = this.upperPlayer;
+
+    this.board.render();
+    this.renderInfo();
+  }
+
+  renderInfo() {
+    this.gameInfoEl?.innerHTML = `#${this.turn}턴 ${this.currentPlayer.type} 차례`;
+  }
+  changeTurn() {
+    this.selectedCell.deactive();
+    this.selectedCell = null;
+
+    if (this.state === 'END') {
+      this.renderInfo(`END! winner is ${this.currentPlayer.type}`);
+    } else {
+      this.turn += 1;
+      this.currentPlayer =
+        this.currentPlayer === this.lowerPlayer
+          ? this.upperPlayer
+          : this.lowerPlayer;
+      this.renderInfo();
+    }
+    this.board.render();
   }
 }
---
+++ catch-lion-ts/src/Player.ts
@@ -0,0 +1,31 @@
+import { Chick, Elephant, Griff, Lion, Piece } from './Piece';
+
+export enum PlayerType {
+  UPPER = 'UPPER',
+  LOWER = 'LOWER',
+}
+
+export class Player {
+  private pieces: Piece[] = [];
+
+  getPieces() {
+    return this.pieces;
+  }
+  constructor(public readonly type: PlayerType) {
+    if (type == PlayerType.UPPER) {
+      this.pieces = [
+        new Griff({ row: 0, col: 0 }, PlayerType.UPPER),
+        new Lion({ row: 0, col: 1 }, PlayerType.UPPER),
+        new Elephant({ row: 0, col: 2 }, PlayerType.UPPER),
+        new Chick({ row: 1, col: 1 }, PlayerType.UPPER),
+      ];
+    } else {
+      this.pieces = [
+        new Elephant({ row: 3, col: 0 }, PlayerType.LOWER),
+        new Lion({ row: 3, col: 1 }, PlayerType.LOWER),
+        new Griff({ row: 3, col: 2 }, PlayerType.LOWER),
+        new Chick({ row: 2, col: 1 }, PlayerType.LOWER),
+      ];
+    }
+  }
+}
