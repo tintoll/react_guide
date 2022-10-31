7142208980657f3a516d774527112268d75c1114
tintoll <belselios@gmail.com>
2022-11-01 08:31:57 +0900

    차트 완성하면서 프로젝트 완료

--- 01-starting-setup/src/components/Chart/Chart.js
+++ 01-starting-setup/src/components/Chart/Chart.js
@@ -1,19 +1,23 @@
-import React from 'react';
-import ChartBar from './ChartBar';
-import './Chart.css'
+import React from "react";
+import ChartBar from "./ChartBar";
+import "./Chart.css";
 
-const Chart = props => {
+const Chart = (props) => {
+  const dataValues = props.dataPoints.map((dataPoint) => dataPoint.value);
+  const totalMaxium = Math.max(...dataValues);
 
-    return (
-        <div className='chart'>
-            {props.dataPoints.map(dataPoint =>
-                <ChartBar
-                    key={dataPoint.label}
-                    value={dataPoint.value}
-                    label={dataPoint.label}
-                    maxValue={null} />)}
-        </div>
-    )
-}
+  return (
+    <div className="chart">
+      {props.dataPoints.map((dataPoint) => (
+        <ChartBar
+          key={dataPoint.label}
+          value={dataPoint.value}
+          label={dataPoint.label}
+          maxValue={totalMaxium}
+        />
+      ))}
+    </div>
+  );
+};
 
 export default Chart;
--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -3,7 +3,7 @@ import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import ExpensesList from "./ExpensesList";
 import "./Expenses.css";
-
+import ExpensesChart from "./ExpensesChart";
 
 function Expenses(props) {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -16,13 +16,13 @@ function Expenses(props) {
     return filteredYear === expense.date.getFullYear().toString();
   });
 
-
   return (
     <Card className="expenses">
       <ExpensesFilter
         selected={filteredYear}
         onChangeFilter={filterChangeHandler}
       />
+      <ExpensesChart expenses={fileteredExpenses} />
       <ExpensesList items={fileteredExpenses} />
     </Card>
   );
