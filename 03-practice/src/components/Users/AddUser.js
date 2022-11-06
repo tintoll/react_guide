b83dedaa366a26bf8c6f3f050d2309abf7550e23
tintoll <belselios@gmail.com>
2022-11-07 08:42:11 +0900

    03-연습 , 사용자 추가 컴포넌트 추가

---
+++ 03-practice/src/components/Users/AddUser.js
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+import Button from "../UI/Button";
+import Card from "../UI/Card";
+
+import calsses from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const addUserHandler = (event) => {
+    event.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      return;
+    }
+
+    if (+enteredAge < 1) {
+      return;
+    }
+
+    console.log(enteredUsername, enteredAge);
+    setEnteredAge("");
+    setEnteredUsername("");
+  };
+
+  const usernameChangeHalder = (event) => {
+    setEnteredUsername(event.target.value);
+  };
+
+  const ageChangeHalder = (event) => {
+    setEnteredAge(event.target.value);
+  };
+
+  return (
+    <Card className={calsses.input}>
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChangeHalder}
+        />
+        <label htmlFor="age">Age (Year)</label>
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChangeHalder}
+        />
+
+        <Button type="submit">Add User</Button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
