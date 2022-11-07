4111533b2ae74f81381af19f2e25973f978c3079
tintoll <belselios@gmail.com>
2022-11-08 08:43:38 +0900

    03-연습, 사용자리스트 및 에러모듈 모달 기능 추가로 완성

--- 03-practice/src/components/Users/AddUser.js
+++ 03-practice/src/components/Users/AddUser.js
@@ -1,24 +1,34 @@
 import React, { useState } from "react";
 import Button from "../UI/Button";
 import Card from "../UI/Card";
+import ErrorModal from "../UI/ErrorModal";
 
 import calsses from "./AddUser.module.css";
 
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
+  const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+      setError({
+        title: "이름 에러",
+        message: "이름을 입력해주세요",
+      });
       return;
     }
 
     if (+enteredAge < 1) {
+      setError({
+        title: "나이 에러",
+        message: "나이를 0이상으로 입력해주세요",
+      });
       return;
     }
 
-    console.log(enteredUsername, enteredAge);
+    props.onAddUser(enteredUsername, enteredAge);
     setEnteredAge("");
     setEnteredUsername("");
   };
@@ -31,27 +41,40 @@ const AddUser = (props) => {
     setEnteredAge(event.target.value);
   };
 
-  return (
-    <Card className={calsses.input}>
-      <form onSubmit={addUserHandler}>
-        <label htmlFor="username">Username</label>
-        <input
-          id="username"
-          type="text"
-          value={enteredUsername}
-          onChange={usernameChangeHalder}
-        />
-        <label htmlFor="age">Age (Year)</label>
-        <input
-          id="age"
-          type="number"
-          value={enteredAge}
-          onChange={ageChangeHalder}
-        />
+  const errorHandler = () => {
+    setError(null);
+  };
 
-        <Button type="submit">Add User</Button>
-      </form>
-    </Card>
+  return (
+    <div>
+      {error && (
+        <ErrorModal
+          onConfirm={errorHandler}
+          title={error.title}
+          message={error.message}
+        />
+      )}
+      <Card className={calsses.input}>
+        <form onSubmit={addUserHandler}>
+          <label htmlFor="username">Username</label>
+          <input
+            id="username"
+            type="text"
+            value={enteredUsername}
+            onChange={usernameChangeHalder}
+          />
+          <label htmlFor="age">Age (Year)</label>
+          <input
+            id="age"
+            type="number"
+            value={enteredAge}
+            onChange={ageChangeHalder}
+          />
+
+          <Button type="submit">Add User</Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
