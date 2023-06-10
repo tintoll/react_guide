62fff6fd6ea40ea61526da5f2bd42653d251a63a
tintoll <tintoll@tintollui-MacBookPro.local>
2023-06-10 22:01:45 +0900

    테스트 추가

--- 26-test/src/components/Greeting.test.js
+++ 26-test/src/components/Greeting.test.js
@@ -1,14 +1,47 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, fireEvent } from "@testing-library/react";
+
 import Greeting from "./Greeting";
-test("render Hello World as a text", () => {
-  // Arrage
-  render(<Greeting />);
 
-  // Act
+describe("Gretting Component", () => {
+  test("render Hello World as a text", () => {
+    // Arrage
+    render(<Greeting />);
+    // Act
+    // Assert
+    // get은 찾지 못하면 에러가 발생
+    // 옵션으로 exact를 줄수도 있음.
+    const helloWorldElement = screen.getByText("Hello World", { exact: false });
+    expect(helloWorldElement).toBeInTheDocument();
+  });
 
-  // Assert
-  // get은 찾지 못하면 에러가 발생
-  // 옵션으로 exact를 줄수도 있음.
-  const helloWorldElement = screen.getByText("Hello World", { exact: false });
-  expect(helloWorldElement).toBeInTheDocument();
+  test('render "good to see you" if click button not clicked', () => {
+    render(<Greeting />);
+    const outputElement = screen.getByText("good to see you", { exact: false });
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('render "Changed!" if click button clicked', () => {
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByText("Change Text!");
+    fireEvent.click(buttonElement);
+
+    const outputElement = screen.getByText("Changed!");
+    expect(outputElement).toBeInTheDocument();
+  });
+
+  test('render is not text "good to see you" if click button clicked', () => {
+    render(<Greeting />);
+
+    // Act
+    const buttonElement = screen.getByText("Change Text!");
+    fireEvent.click(buttonElement);
+
+    // query는 결과값이 없으면 null을 리턴
+    const outputElement = screen.queryByText("good to see you", {
+      exact: false,
+    });
+    expect(outputElement).toBeNull();
+  });
 });
