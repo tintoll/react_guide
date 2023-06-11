c41990d574915059f65fa9f920596f99b00e46d1
tintoll <belselios@gmail.com>
2023-06-11 22:34:04 +0900

    add random name

--- 29-summary/src/components/Post.jsx
+++ 29-summary/src/components/Post.jsx
@@ -1,7 +1,10 @@
+const names = ["Maximilian", "Manuel"];
+
 function Post() {
+  const chosenName = Math.random() > 0.5 ? names[0] : names[1];
   return (
     <div>
-      <p>Blue</p>
+      <p>{chosenName}</p>
       <p>React.js is assume</p>
     </div>
   );
