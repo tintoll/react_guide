42b2b3dd3499a08e14f2653b9e9d333f441566ec
tintoll <belselios@gmail.com>
2023-06-13 06:32:59 +0900

    상트 끌어올리기

--- 29-summary/src/components/NewPost.jsx
+++ 29-summary/src/components/NewPost.jsx
@@ -1,23 +1,16 @@
 import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-function NewPost() {
-  const [endteredBody, setEnteredBody] = useState("");
-
-  function changeBodyHandler(event) {
-    setEnteredBody(event.target.value);
-  }
-
+function NewPost(props) {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={changeBodyHandler} />
+        <textarea id="body" required rows={3} onChange={props.onChangeBody} />
       </p>
-      <p>{endteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onChangeAuthor} />
       </p>
     </form>
   );
--- 29-summary/src/components/PostList.jsx
+++ 29-summary/src/components/PostList.jsx
@@ -1,12 +1,28 @@
 import NewPost from "./NewPost";
 import Post from "./Post";
+import { useState } from "react";
 import classes from "./PostList.module.css";
+
 export default function PostList() {
+  const [endteredBody, setEnteredBody] = useState("");
+  const [endteredAuthor, setEnteredAuthor] = useState("");
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onChangeBody={changeBodyHandler}
+        onChangeAuthor={changeAuthorHandler}
+      />
       <ul className={classes.posts}>
-        <Post author="Max" body="React" />
+        <Post author={endteredAuthor} body={endteredBody} />
         <Post author="Hani" body="Vue" />
       </ul>
     </>
