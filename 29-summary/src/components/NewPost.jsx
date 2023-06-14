67cfa212499f38f0552be56ebbbecb606b588919
tintoll <belselios@gmail.com>
2023-06-15 05:52:11 +0900

    post list 관리하도록 변경

--- 29-summary/src/components/NewPost.jsx
+++ 29-summary/src/components/NewPost.jsx
@@ -1,15 +1,36 @@
+import { useState } from "react";
 import classes from "./NewPost.module.css";
 
-function NewPost({ onChangeBody, onChangeName, onCancel }) {
+function NewPost({ onCancel, onAddPost }) {
+  const [endteredBody, setEnteredBody] = useState("");
+  const [endteredAuthor, setEnteredAuthor] = useState("");
+
+  function changeBodyHandler(event) {
+    setEnteredBody(event.target.value);
+  }
+
+  function changeAuthorHandler(event) {
+    setEnteredAuthor(event.target.value);
+  }
+  function submitHandler(event) {
+    event.preventDefault();
+    const postData = {
+      body: endteredBody,
+      author: endteredAuthor,
+    };
+
+    onAddPost(postData);
+    onCancel();
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} onChange={onChangeBody} />
+        <textarea id="body" required rows={3} onChange={changeAuthorHandler} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required onChange={onChangeName} />
+        <input type="text" id="name" required onChange={changeBodyHandler} />
       </p>
       <p className={classes.actions}>
         <button type="button" onClick={onCancel}>
--- 29-summary/src/components/PostList.jsx
+++ 29-summary/src/components/PostList.jsx
@@ -5,33 +5,31 @@ import classes from "./PostList.module.css";
 import Modal from "./Modal";
 
 export default function PostList({ isPosting, onStopPosting }) {
-  const [endteredBody, setEnteredBody] = useState("");
-  const [endteredAuthor, setEnteredAuthor] = useState("");
-
-  function changeBodyHandler(event) {
-    setEnteredBody(event.target.value);
+  const [posts, setPosts] = useState([]);
+  function addPostHandler(postData) {
+    setPosts((existingPosts) => [postData, ...existingPosts]);
   }
-
-  function changeAuthorHandler(event) {
-    setEnteredAuthor(event.target.value);
-  }
-
   return (
     <>
       {isPosting && (
         <Modal onClose={onStopPosting}>
-          <NewPost
-            onChangeBody={changeBodyHandler}
-            onChangeAuthor={changeAuthorHandler}
-            onCancel={onStopPosting}
-          />
+          <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
+      {posts.length > 0 && (
+        <ul className={classes.posts}>
+          {posts.map((post) => (
+            <Post key={post.body} author={post.author} body={post.body} />
+          ))}
+        </ul>
+      )}
 
-      <ul className={classes.posts}>
-        <Post author={endteredAuthor} body={endteredBody} />
-        <Post author="Hani" body="Vue" />
-      </ul>
+      {posts.length === 0 && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <h2>There are no posts yet.</h2>
+          <p>Start adding some!</p>
+        </div>
+      )}
     </>
   );
 }
