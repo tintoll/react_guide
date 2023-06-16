03c00a1b432976573e60ac06b588a8163564a837
tintoll <belselios@gmail.com>
2023-06-17 06:13:28 +0900

    백엔드에서 목록 속도 느리도록 설정하여 로딩상태 추가하여 처리

--- 29-summary/src/components/PostList.jsx
+++ 29-summary/src/components/PostList.jsx
@@ -6,13 +6,16 @@ import Modal from "./Modal";
 
 export default function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
-  console.log("aa");
+  const [isFetching, setIsFetching] = useState(false);
+
   useEffect(() => {
     // async를 사용하기 위해서 함수를 만들었음.
     async function fetchPost() {
+      setIsFetching(true);
       const response = await fetch("http://localhost:8080/posts");
       const resData = await response.json();
       setPosts(resData.posts);
+      setIsFetching(false);
     }
     fetchPost();
   }, []);
@@ -34,7 +37,7 @@ export default function PostList({ isPosting, onStopPosting }) {
           <NewPost onCancel={onStopPosting} onAddPost={addPostHandler} />
         </Modal>
       )}
-      {posts.length > 0 && (
+      {!isFetching && posts.length > 0 && (
         <ul className={classes.posts}>
           {posts.map((post) => (
             <Post key={post.body} author={post.author} body={post.body} />
@@ -42,10 +45,15 @@ export default function PostList({ isPosting, onStopPosting }) {
         </ul>
       )}
 
-      {posts.length === 0 && (
+      {!isFetching && posts.length === 0 && (
         <div style={{ textAlign: "center", color: "white" }}>
           <h2>There are no posts yet.</h2>
           <p>Start adding some!</p>
+        </div>
+      )}
+      {isFetching && (
+        <div style={{ textAlign: "center", color: "white" }}>
+          <p>Loading post ....</p>
         </div>
       )}
     </>
