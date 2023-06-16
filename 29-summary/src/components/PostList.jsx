5a93cce5c60a8a43ee903560e898e205d997d3e4
tintoll <belselios@gmail.com>
2023-06-17 06:07:36 +0900

    백엔드 서버와 연동해서 목록가져오기

--- 29-summary/src/components/PostList.jsx
+++ 29-summary/src/components/PostList.jsx
@@ -1,12 +1,30 @@
 import NewPost from "./NewPost";
 import Post from "./Post";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import classes from "./PostList.module.css";
 import Modal from "./Modal";
 
 export default function PostList({ isPosting, onStopPosting }) {
   const [posts, setPosts] = useState([]);
+  console.log("aa");
+  useEffect(() => {
+    // async를 사용하기 위해서 함수를 만들었음.
+    async function fetchPost() {
+      const response = await fetch("http://localhost:8080/posts");
+      const resData = await response.json();
+      setPosts(resData.posts);
+    }
+    fetchPost();
+  }, []);
+
   function addPostHandler(postData) {
+    fetch("http://localhost:8080/post", {
+      method: "POST",
+      body: postData,
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
     setPosts((existingPosts) => [postData, ...existingPosts]);
   }
   return (
