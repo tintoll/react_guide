2c36604b5721d4bcfaf830950ba71bbfa928ddc8
tintoll <belselios@gmail.com>
2023-06-19 22:07:47 +0900

    post 상세보기 추가

--- 29-summary/src/main.jsx
+++ 29-summary/src/main.jsx
@@ -5,6 +5,7 @@ import "./index.css";
 import NewPost, { action as postAction } from "./routes/NewPost";
 import RootLayout from "./routes/RootLayout";
 import Posts, { loader as postLoader } from "./routes/Posts";
+import PostDetails, { loader as postDetailLoader } from "./routes/PostDetails";
 
 const router = createBrowserRouter([
   {
@@ -17,6 +18,7 @@ const router = createBrowserRouter([
         loader: postLoader,
         children: [
           { path: "/create-post", element: <NewPost />, action: postAction },
+          { path: "/:id", element: <PostDetails />, loader: postDetailLoader },
         ],
       },
     ],
