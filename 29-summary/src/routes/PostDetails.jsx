f17244acbf050d2e3a14fc4b2a50c5a67de8251b
tintoll <belselios@gmail.com>
2023-06-19 22:09:06 +0900

    오타 수정

--- 29-summary/src/routes/PostDetails.jsx
+++ 29-summary/src/routes/PostDetails.jsx
@@ -34,7 +34,7 @@ function PostDetails() {
 export default PostDetails;
 
 export async function loader({ params }) {
-  const response = await fetch("http://localhost:8080/posts" + params.id);
+  const response = await fetch("http://localhost:8080/posts/" + params.id);
   const resData = await response.json();
   return resData.post;
 }
