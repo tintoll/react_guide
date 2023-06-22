354a147e30d5bc540bc48050ee69dadea9237729
tintoll <belselios@gmail.com>
2023-06-23 05:18:27 +0900

    useEffect 사용해서 처리하기

--- 14-http-request/src/App.js
+++ 14-http-request/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import MoviesList from "./components/MoviesList";
 import "./App.css";
@@ -8,12 +8,11 @@ function App() {
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
-  // async - await를 활용하는게 보기 좋다.
-  async function fetchMoviesHandler() {
+  const fetchMoviesHandler = useCallback(async () => {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch("https://swapi.dev/api/flms/");
+      const response = await fetch("https://swapi.dev/api/films/");
 
       if (!response.ok) {
         throw new Error("Something went wrong");
@@ -33,7 +32,12 @@ function App() {
       setError(error.message);
     }
     setIsLoading(false);
-  }
+  }, []);
+
+  // useEffect에 의존을 주는게 좋은데 함수는 사용할수 없다. 그래서 useCallback으로 감
+  useEffect(() => {
+    fetchMoviesHandler();
+  }, [fetchMoviesHandler]);
 
   let content = <p>Found no movies</p>;
   if (movies.length > 0) {
