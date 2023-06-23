f8caec7356a89bd9c5b1d7b9c6432490a18c48d2
tintoll <belselios@gmail.com>
2023-06-24 05:24:34 +0900

    여러 곳에서 사용되로록 커스텀훅에 조건 추가

--- 15-custom-hook/src/hooks/use-counter.js
+++ 15-custom-hook/src/hooks/use-counter.js
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 
-const useCounter = () => {
+const useCounter = (forwrad = true) => {
   const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
-      setCounter((prevCounter) => prevCounter + 1);
+      if (forwrad) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
     }, 1000);
 
     return () => clearInterval(interval);
-  }, []);
+  }, [forwrad]);
 
   return counter;
 };
