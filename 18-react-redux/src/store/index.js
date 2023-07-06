bc77dc390b389b3e3ea226541a6976c65ed0e763
tintoll <belselios@gmail.com>
2023-07-06 22:23:49 +0900

    리덕스에서 여러개의 상태 사용하기

--- 18-react-redux/src/store/index.js
+++ 18-react-redux/src/store/index.js
@@ -1,21 +1,33 @@
 import { createStore } from "redux";
 
-const counterReducer = (state = { counter: 0 }, action) => {
+const initState = { counter: 0, showCounter: true };
+
+const counterReducer = (state = initState, action) => {
   if (action.type === "increment") {
     return {
+      ...state,
       counter: state.counter + 1,
     };
   }
 
   if (action.type === "increase") {
     return {
+      ...state,
       counter: state.counter + action.amount,
     };
   }
 
   if (action.type === "decrement") {
     return {
+      ...state,
       counter: state.counter - 1,
+    };
+  }
+
+  if (action.type === "toggle") {
+    return {
+      ...state,
+      showCounter: !state.showCounter,
     };
   }
 
