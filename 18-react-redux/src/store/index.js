bbbca9d13e00571f4715c873e8abf6614b6ce659
tintoll <belselios@gmail.com>
2023-07-07 04:53:03 +0900

    리덕스 툴킷 설치 및 counter slice 만들기

--- 18-react-redux/src/store/index.js
+++ 18-react-redux/src/store/index.js
@@ -1,6 +1,26 @@
 import { createStore } from "redux";
+import { createSlice } from "@reduxjs/toolkit";
 
 const initState = { counter: 0, showCounter: true };
+
+createSlice({
+  name: "counter",
+  initState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    increase(state) {
+      state.counter--;
+    },
+    decrement(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggleCounter(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
 const counterReducer = (state = initState, action) => {
   if (action.type === "increment") {
