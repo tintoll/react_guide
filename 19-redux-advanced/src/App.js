0405f8e7d5f55ca8eb970a3e550d7ffe3d936ee7
tintoll <belselios@gmail.com>
2023-07-11 22:09:20 +0900

    useEffect를 이용하여 cart 데이터 firebase에 저장하는 부분 추가

--- 19-redux-advanced/src/App.js
+++ 19-redux-advanced/src/App.js
@@ -2,9 +2,22 @@ import { useSelector } from "react-redux";
 import Cart from "./components/Cart/Cart";
 import Layout from "./components/Layout/Layout";
 import Products from "./components/Shop/Products";
+import { useEffect } from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+
+  // cart에 대한 정보를 리덕스에서 가져오고 cart가 변경될때마다 백엔드로 저장해주면
+  // 실제로는 리덕스 처리는 리덕스하고 비동기는 useEffect를 이용하여 처리할 수 있다.
+  // 하지만 이것은 문제가 있다.
+  const cart = useSelector((state) => state.cart);
+  useEffect(() => {
+    fetch("https:///cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
