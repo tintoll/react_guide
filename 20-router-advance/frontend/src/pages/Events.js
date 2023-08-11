e09fb3ced91577eea71e4c74bf9963c8d7e4eb10
tintoll <belselios@gmail.com>
2023-08-11 22:23:19 +0900

    json 함수를 이용하여 throw 하기

--- 20-router-advance/frontend/src/pages/Events.js
+++ 20-router-advance/frontend/src/pages/Events.js
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
 
 import EventsList from "../components/EventsList";
 
@@ -10,13 +10,21 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
-  const response = await fetch("http://localhost:8080/eventsaa");
+  const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
     // 여기에 에러관련 내용을 넘기자
     // return {isError:true, message: "Could not fetch events."};
-    throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
-      status: 500,
-    });
+    // throw new Response(JSON.stringify({ message: "Could not fetch events." }), {
+    //   status: 500,
+    // });
+
+    // Response를 간편하게 설정할수 있게 해주는 json Util함수
+    throw json(
+      { message: "Could not fetch events." },
+      {
+        status: 500,
+      }
+    );
   } else {
     const resData = await response.json();
     return resData.events;
