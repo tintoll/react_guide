1bb47512b141ba4856ab185641afaad328bb9b0d
tintoll <belselios@gmail.com>
2023-08-13 15:10:40 +0900

    detail page에 loader적용

--- 20-router-advance/frontend/src/App.js
+++ 20-router-advance/frontend/src/App.js
@@ -1,7 +1,9 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
-import EventDetailPage from "./pages/EventDetail";
+import EventDetailPage, {
+  loader as eventDetailLoader,
+} from "./pages/EventDetail";
 import NewEventPage from "./pages/NewEvent";
 import EditEventPage from "./pages/EditEvent";
 import RootLaout from "./pages/RootLayout";
@@ -30,6 +32,7 @@ const router = createBrowserRouter([
           {
             path: ":eventId",
             element: <EventDetailPage />,
+            loader: eventDetailLoader,
           },
           {
             path: "new",
--- 20-router-advance/frontend/src/pages/EventDetail.js
+++ 20-router-advance/frontend/src/pages/EventDetail.js
@@ -1,14 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useLoaderData, json } from "react-router-dom";
+import EventItem from "../components/EventItem";
 
 function EventDetailPage() {
-  const params = useParams();
+  const data = useLoaderData();
 
-  return (
-    <>
-      <h1> Event Detail page</h1>
-      <p> id : {params.eventId}</p>
-    </>
-  );
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
+
+export async function loader({ request, params }) {
+  const id = params.eventId;
+
+  const response = await fetch("http://localhost:8080/events/" + id);
+
+  if (!response.ok) {
+    throw json({ message: "Could not fetch event detail" }, { status: 500 });
+  } else {
+    return response;
+  }
+}
