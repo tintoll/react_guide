f5cb0ccb870e4b063b652feab755973e0aff0c09
tintoll <belselios@gmail.com>
2023-09-04 22:15:41 +0900

    로그아웃 메뉴 추가

--- 21-authetication/frontend/src/App.js
+++ 21-authetication/frontend/src/App.js
@@ -17,6 +17,8 @@ import AuthenticationPage, {
   action as authAction,
 } from "./pages/Authentication";
 
+import { action as logoutAction } from "./pages/Logout";
+
 const router = createBrowserRouter([
   {
     path: "/",
@@ -66,6 +68,10 @@ const router = createBrowserRouter([
         path: "newsletter",
         element: <NewsletterPage />,
         action: newsletterAction,
+      },
+      {
+        path: "logout",
+        action: logoutAction,
       },
     ],
   },
