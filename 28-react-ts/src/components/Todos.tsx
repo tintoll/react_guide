849d75a3b568feb3a5570d064c3389c26ab5bd8e
tintoll <belselios@gmail.com>
2023-09-07 22:38:09 +0900

    TodoItem 추가

--- 28-react-ts/src/components/Todos.tsx
+++ 28-react-ts/src/components/Todos.tsx
@@ -1,13 +1,14 @@
 // props에 대한 타입을 정의하면 아래와 같이 React.FC로 타입일 설정해줘야한다.
 
 import Todo from "../models/todo";
+import TodoItem from "./TodoItem";
 
 // 추가되는 props가 있는 경우에는 <{}> 안에 정의해주면 된다.
 const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item.id}>{item.text}</li>
+        <TodoItem key={item.id} text={item.text} />
       ))}
     </ul>
   );
