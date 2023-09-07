caafd16fd4cb6c93a9c711926495334491487a38
tintoll <belselios@gmail.com>
2023-09-07 22:22:13 +0900

    Todo 클래스를 만들어서 타입으로 사용

--- 28-react-ts/src/components/Todos.tsx
+++ 28-react-ts/src/components/Todos.tsx
@@ -1,10 +1,13 @@
 // props에 대한 타입을 정의하면 아래와 같이 React.FC로 타입일 설정해줘야한다.
+
+import Todo from "../models/todo";
+
 // 추가되는 props가 있는 경우에는 <{}> 안에 정의해주면 된다.
-const Todos: React.FC<{ items: string[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((item) => (
-        <li key={item}>{item}</li>
+        <li key={item.id}>{item.text}</li>
       ))}
     </ul>
   );
