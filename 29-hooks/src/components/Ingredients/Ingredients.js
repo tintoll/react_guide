884dc2299fc82862c62e5b32bef08b766da94278
tintoll <belselios@gmail.com>
2023-09-09 23:31:36 +0900

    ingredient form 관련 state 작업 추가

--- 29-hooks/src/components/Ingredients/Ingredients.js
+++ 29-hooks/src/components/Ingredients/Ingredients.js
@@ -1,16 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
 
-import IngredientForm from './IngredientForm';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import Search from "./Search";
+import IngredientList from "./IngredientList";
 
 function Ingredients() {
+  const [ingredients, setIngredients] = useState([]);
+
+  const addIngredientHandler = (ingredient) => {
+    setIngredients((prevIngredients) => [
+      ...prevIngredients,
+      { id: Math.random().toString(), ...ingredient },
+    ]);
+  };
+
+  const removeIngredientHandler = (id) => {
+    setIngredients((prevIngredients) =>
+      prevIngredients.filter((ingredient) => ingredient.id !== id)
+    );
+  };
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIgredient={addIngredientHandler} />
 
       <section>
         <Search />
-        {/* Need to add list here! */}
+        <IngredientList
+          ingredients={ingredients}
+          onRemoveItem={removeIngredientHandler}
+        />
       </section>
     </div>
   );
