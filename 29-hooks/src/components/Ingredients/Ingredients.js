ec8bb218aab3e70fb69c0cd7008011b6235d036b
tintoll <belselios@gmail.com>
2023-09-16 06:21:07 +0900

    useState to useReducer

--- 29-hooks/src/components/Ingredients/Ingredients.js
+++ 29-hooks/src/components/Ingredients/Ingredients.js
@@ -1,19 +1,33 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useReducer, useState } from "react";
 
 import IngredientForm from "./IngredientForm";
 import Search from "./Search";
 import IngredientList from "./IngredientList";
 import ErrorModal from "../UI/ErrorModal";
 
+const ingredientReducer = (currentIngredients, action) => {
+  switch (action.type) {
+    case "SET":
+      return action.ingredients;
+    case "ADD":
+      return [...currentIngredients, action.ingredient];
+    case "DELETE":
+      return currentIngredients.filter((ing) => ing.id !== action.id);
+    default:
+      throw new Error("Shoud not get there!");
+  }
+};
+
 function Ingredients() {
-  const [ingredients, setIngredients] = useState([]);
+  const [userIngredients, dispatch] = useReducer(ingredientReducer, []);
+  // const [ingredients, setIngredients] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState();
 
   const addIngredientHandler = (ingredient) => {
     setIsLoading(true);
     fetch(
-      "https://react-test-hooks-3ab7a-default-rtdb.firebaseio.com/ingredients.jsn",
+      "https://react-test-hooks-3ab7a-default-rtdb.firebaseio.com/ingredients.json",
       {
         method: "POST",
         body: JSON.stringify(ingredient),
@@ -27,10 +41,14 @@ function Ingredients() {
         return res.json();
       })
       .then((resData) => {
-        setIngredients((prevIngredients) => [
-          ...prevIngredients,
-          { id: resData.name, ...ingredient },
-        ]);
+        // setIngredients((prevIngredients) => [
+        //   ...prevIngredients,
+        //   { id: resData.name, ...ingredient },
+        // ]);
+        dispatch({
+          type: "ADD",
+          ingredient: { id: resData.name, ...ingredient },
+        });
       })
       .catch((error) => {
         setError(error.message);
@@ -41,7 +59,8 @@ function Ingredients() {
   // 이 함수는 새로 생성이 되니 자식컴포넌트에서는 새로운걸로 판단되 리렌더링이 된다.
   // uesCallback을 하면 캐싱을 하기 때문에 함수가 새로 생성되지 않는다.
   const fileteredIngredientsHandler = useCallback((filteredIngredients) => {
-    setIngredients(filteredIngredients);
+    // setIngredients(filteredIngredients);
+    dispatch({ type: "SET", ingredients: filteredIngredients });
   }, []);
 
   const removeIngredientHandler = (id) => {
@@ -54,9 +73,10 @@ function Ingredients() {
     )
       .then((res) => {
         setIsLoading(false);
-        setIngredients((prevIngredients) =>
-          prevIngredients.filter((ingredient) => ingredient.id !== id)
-        );
+        // setIngredients((prevIngredients) =>
+        //   prevIngredients.filter((ingredient) => ingredient.id !== id)
+        // );
+        dispatch({ type: "DELETE", id });
       })
       .catch((error) => {
         setError(error.message);
@@ -78,7 +98,7 @@ function Ingredients() {
       <section>
         <Search onLoadIngredients={fileteredIngredientsHandler} />
         <IngredientList
-          ingredients={ingredients}
+          ingredients={userIngredients}
           onRemoveItem={removeIngredientHandler}
         />
       </section>
